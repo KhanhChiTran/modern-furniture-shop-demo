51cbf2fb87f48190cc31851c85061ba10abe06c5
Chi Tran <khanhchi.tran13051@gmail.com>
2021-04-26 23:56:24 +0200

    add reviews section in HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { CgArrowLongLeft, CgArrowLongRight } from "react-icons/cg";
+
 import Layout from "../components/Layout";
-import "./home.scss";
+
 import BambooPot from "../assets/images/bamboo_pot.jpeg";
-import Pot2 from "../assets/images/pot2.jpeg";
+import Basket from "../assets/images/basket1.jpeg";
 import Table1 from "../assets/images/table1.jpeg";
+
+import Users from "../assets/data/data";
+
+import "./home.scss";
+
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const { id, image, job, name, text } = Users[index];
   return (
     <Layout>
       <div className="home-wrap">
@@ -12,19 +21,29 @@ export default function Home() {
           {/* <img className="pot" src={Pot1} alt="Pot" /> */}
           <img className="table" src={Table1} alt="Table" />
           <img className="pot" src={BambooPot} alt="Pot" />
-          <img className="pot2" src={Pot2} alt="Vase" />
+          <img className="pot2" src={Basket} alt="Vase" />
         </div>
         <div className="home-intro">
-          <h1>Home Decoration</h1>
-          <p>
-            Want to reinvent the look and feel of your home? Our stylish range
-            of home decorations adds those finishing touches to any room – think
-            everything from fragrant scented candles, to elegant glassware and
-            chic storage options. Stay on trend with rattan baskets, or
-            transform your space into an urban jungle with the help of our
-            indoor and outdoor plant pots. Whatever style you’re going for, our
-            edit is here to help you fall head over heels with staying at home.
-          </p>
+          <h3>Home Decoration</h3>
+          <article>
+            Want to reinvent the look and feel of your home? Whatever style
+            you’re going for, our edit is here to help you fall head over heels
+            with staying at home.
+          </article>
+        </div>
+        <div className="reviews">
+          <button className="arrow-left">
+            {" "}
+            <CgArrowLongLeft />
+          </button>
+          <button className="arrow-right">
+            {" "}
+            <CgArrowLongRight />
+          </button>
+          <img className="user-img" src={image} alt="" />
+          <p>{text}</p>
+          <h2>{name.toUpperCase()}</h2>
+          <h4>{job}</h4>
         </div>
       </div>
     </Layout>
