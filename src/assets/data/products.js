1014b9853f6d0a0b90713550e50d25750d62dc49
Chi Tran <khanhchi.tran13051@gmail.com>
2021-04-27 21:30:19 +0200

    add single product page

--- src/assets/data/products.js
+++ src/assets/data/products.js
@@ -1,11 +1,13 @@
 const Products = [
   {
     id: 1,
-    name: "TABLE LAMP",
+    name: "TABLE LAMPA",
     img:
       "https://www.ikea.com/de/de/images/products/gamlared-tisch-antikbeize-hell-schwarz-gebeizt__0737080_pe740872_s5.jpg?f=xxxs",
     star: 5,
     price: 120,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 2,
@@ -24,6 +26,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/brunsta-hemma-haengeleuchte-schwarz__0669228_pe714904_s5.jpg?f=xs",
     star: 5,
     price: 150,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 4,
@@ -32,6 +36,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/stursk-wanduhr-schwarz__0661139_pe711350_s5.jpg?f=xxxs",
     star: 4,
     price: 23,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 5,
@@ -40,6 +46,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/nordby-tisch-gummibaum__0157063_pe315528_s5.jpg?f=xxxs",
     star: 5,
     price: 210,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 6,
@@ -48,6 +56,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/sockeraert-vase-weiss__0091175_pe226468_s5.jpg?f=xxxs",
     star: 5,
     price: 49,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 7,
@@ -56,6 +66,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/lustigkurre-korb-naturfarben-seegras__0922225_pe788093_s5.jpg?f=xxxs",
     star: 4,
     price: 19,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 8,
@@ -64,6 +76,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/kransborre-kissen-hellrosa__0920477_pe787064_s5.jpg?f=xxxs",
     star: 5,
     price: 19,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 9,
@@ -72,6 +86,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/fejka-topfpflanze-kuenstl-topf-3er-set-drinnen-draussen-gruen__0898071_pe782543_s5.jpg?f=xxxs",
     star: 5,
     price: 39,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 10,
@@ -80,6 +96,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/kapkrusbaer-ampel-graugruen__0919747_pe789482_s5.jpg?f=xxxs",
     star: 5,
     price: 9,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
   {
     id: 11,
@@ -96,6 +114,8 @@ const Products = [
       "https://www.ikea.com/de/de/images/products/kapkrusbaer-ampel-graugruen__0919747_pe789482_s5.jpg?f=xxxs",
     star: 5,
     price: 9,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
   },
 ];
 export default Products;
--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -3,23 +3,34 @@ import Layout from "../../components/layout/Layout";
 
 import Rating from "../../components/rating/Rating";
 import { Link } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
 
 import "./product.scss";
 
 export default function ProductDetail({ location }) {
-  const { img, name, star, price } = location.state;
+  const { img, name, star, price, description } = location.state;
   return (
     <Layout>
       <div className="item-info">
         <div className="item-img">
-          <img src={img} alt="" />
+          <img src={img} alt="deco" />
         </div>
         <div className="item-text">
           <h2>{name}</h2>
           <p className="item-price">{price}€</p>
           <Rating star={star} />
+          <p>{description}</p>
           <div className="item-cart">
-            <input type="text" />
+            <button className="item-btn cart-btn" type="submit">
+              ADD TO CART
+            </button>
+            <button className="item-btn wishlist-btn" type="submit">
+              ADD TO WISHLIST{" "}
+              <span className="icon">
+                {" "}
+                <AiOutlineHeart />
+              </span>{" "}
+            </button>
           </div>
         </div>
       </div>
