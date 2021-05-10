5abfd4173b3159fe7efeb5ec7c69c3cbce8e9ee9
Chi Tran <khanhchi.tran13051@gmail.com>
2021-05-10 08:25:39 +0200

    remove src and try again   the letter case sensitivity

--- src/firebase/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCbWn9FwLhZhuo-9iaf2STZMnN2bV27Rw4",
-  authDomain: "shop-demo-f5018.firebaseapp.com",
-  projectId: "shop-demo-f5018",
-  storageBucket: "shop-demo-f5018.appspot.com",
-  messagingSenderId: "287584322018",
-  appId: "1:287584322018:web:ea3fbd799cecfc935779bb",
-};
--- src/reducer/productTypes.js
+++
@@ -1,7 +0,0 @@
-export const productTypes = {
-    ADD_TO_CART : 'ADD_TO_CART',
-    MINUS_FROM_CART : 'MINUS_FROM_CART',
-    REMOVE_FROM_CART : 'REMOVE_FROM_CART',
-    TOGGLE_WISHLIST :'TOGGLE_WISHLIST',
-    
-}
