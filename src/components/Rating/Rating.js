66904dc99c4867bb5e911a535d26b8fef51d7378
Chi Tran <khanhchi.tran13051@gmail.com>
2021-05-17 18:51:47 +0200

    thinking about React.memo()

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { AiFillStar } from "react-icons/ai";
-export default function Rating({ star = 5 }) {
-  let ratingArray = [...new Array(star)];
-
+import React, { memo } from "react"
+import { AiFillStar } from "react-icons/ai"
+const x = {
+  hello: "xin chao",
+}
+x.hello = "nihao"
+function Rating({ star = 5, setX }) {
+  let ratingArray = [...new Array(star)]
+  console.log("rating is rerendering ...")
   return (
     <div>
       {ratingArray.map((rate, index) => (
         <span key={index}> {<AiFillStar className="star" />} </span>
       ))}
     </div>
-  );
+  )
 }
+console.log(x)
+// THINKING : Do i really need this memo() ?
+export default memo(Rating)
